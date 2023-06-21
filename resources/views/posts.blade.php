45e2a73e1b2957eab9db64d4165b6a89cdb2a2a2
naufal-mi <naufal.mi.9999@gmail.com>
2023-06-21 09:57:51 +0700

    Update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,9 +3,13 @@
 @section('container')
     
     @foreach ($posts as $post)
-        <h2>{{ $post["title"] }}</h2>
+    <article class="mb-5">
+        <h2>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+        </h2>
         <h5>{{ $post["author"] }}</h5>
         <p>{{ $post["body"] }}</p>
+    </article>
     @endforeach
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,13 @@ Route::get('/blog', function () {
     $blog_posts = [
         [
             "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
             "author" => "Naufal MI",
             "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae blanditiis molestiae eveniet inventore autem corporis impedit sit laborum perferendis optio et dicta maxime minima iste nulla quaerat, rem laboriosam dolorem? Nesciunt veritatis deserunt autem a quidem, adipisci inventore obcaecati delectus excepturi nemo esse cumque consequuntur! Consequuntur, dicta aspernatur dolor facere optio dolorem magnam adipisci molestiae id quia omnis culpa sed labore incidunt voluptatum ducimus iusto? Veritatis adipisci inventore perferendis, dolorem, tempora laboriosam ducimus temporibus amet non cupiditate, incidunt dolor minima?",
         ],
         [
             "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
             "author" => "Valll",
             "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae blanditiis molestiae eveniet inventore autem corporis impedit sit laborum perferendis optio et dicta maxime minima iste nulla quaerat, rem laboriosam dolorem? Nesciunt veritatis deserunt autem a quidem, adipisci inventore obcaecati delectus excepturi nemo esse cumque consequuntur! Consequuntur, dicta aspernatur dolor facere optio dolorem magnam adipisci molestiae id quia omnis culpa sed labore incidunt voluptatum ducimus iusto? Veritatis adipisci inventore perferendis, dolorem, tempora laboriosam ducimus temporibus amet non cupiditate, incidunt dolor minima?",
         ],
@@ -44,5 +46,34 @@ Route::get('/blog', function () {
     return view('posts', [
         "title" => "Posts",
         "posts" => $blog_posts,
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Naufal MI",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae blanditiis molestiae eveniet inventore autem corporis impedit sit laborum perferendis optio et dicta maxime minima iste nulla quaerat, rem laboriosam dolorem? Nesciunt veritatis deserunt autem a quidem, adipisci inventore obcaecati delectus excepturi nemo esse cumque consequuntur! Consequuntur, dicta aspernatur dolor facere optio dolorem magnam adipisci molestiae id quia omnis culpa sed labore incidunt voluptatum ducimus iusto? Veritatis adipisci inventore perferendis, dolorem, tempora laboriosam ducimus temporibus amet non cupiditate, incidunt dolor minima?",
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Valll",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae blanditiis molestiae eveniet inventore autem corporis impedit sit laborum perferendis optio et dicta maxime minima iste nulla quaerat, rem laboriosam dolorem? Nesciunt veritatis deserunt autem a quidem, adipisci inventore obcaecati delectus excepturi nemo esse cumque consequuntur! Consequuntur, dicta aspernatur dolor facere optio dolorem magnam adipisci molestiae id quia omnis culpa sed labore incidunt voluptatum ducimus iusto? Veritatis adipisci inventore perferendis, dolorem, tempora laboriosam ducimus temporibus amet non cupiditate, incidunt dolor minima?",
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        'title' => "Single Post",
+        "post" => $new_post,
     ]);
 });
